cd28291b86e0dd1a452eedf54df588e0a73fd964
santoshpasi23 <santosh@coursera-something.com>
2024-02-09 12:48:28 +0530

    File update version2

---
+++ app.js
@@ -0,0 +1,40 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope, $filter){
+
+$scope.text = "";
+$scope.message = "Please enter data first";
+$scope.CheckIt = function (){
+  if($scope.text.length > 0){
+
+      $scope.message = CheckIfTooMuch($scope.text);
+  }else{
+      $scope.message= "Please enter data first";
+  }
+
+};
+
+function CheckIfTooMuch(string){
+var words = string.split(',');
+
+if(words.length <= 3){
+  return "Enjoy!";
+}else{
+    return "Too Much!";
+}
+
+
+}
+
+
+
+}
+
+
+})();

dbce7b3a8061696c1ade1d20a602c722a03d1860
santoshpasi23 <santosh@coursera-something.com>
2024-02-09 14:56:59 +0530

    Add font color in the massage

--- app.js
+++ app.js
@@ -3,24 +3,27 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 LunchCheckController.$inject = ['$scope'];
+
 function LunchCheckController ($scope, $filter){
 $scope.text = "";
 $scope.message = "Please Enter Data Then Click on Button 'Check If Too Much' ";
+
 $scope.CheckIt = function (){
   if($scope.text.length > 0){
-
       $scope.message = CheckIfTooMuch($scope.text);
   }else{
       $scope.message= "Please Enter Data Then Click on Button 'Check If Too Much'";
   }
 };
+
 function CheckIfTooMuch(string){
 var words = string.split(',');
 if(words.length <= 3){
-  return "Enjoy!";
+    return "Enjoy!";
 }else{
     return "Too Much!";
 }
 }
 }
+
 })();

4ed8932ff2d82bf3efd195b7eeac75968c01268f
santoshpasi23 <santosh@coursera-something.com>
2024-02-09 13:03:34 +0530

    version3

--- app.js
+++ app.js
@@ -1,40 +1,26 @@
 (function () {
 'use strict';
-
 angular.module('LunchCheck', [])
-
 .controller('LunchCheckController', LunchCheckController);
 LunchCheckController.$inject = ['$scope'];
-
 function LunchCheckController ($scope, $filter){
-
 $scope.text = "";
-$scope.message = "Please enter data first";
+$scope.message = "Please Enter Data Then Click on Button 'Check If Too Much' ";
 $scope.CheckIt = function (){
   if($scope.text.length > 0){
 
       $scope.message = CheckIfTooMuch($scope.text);
   }else{
-      $scope.message= "Please enter data first";
+      $scope.message= "Please Enter Data Then Click on Button 'Check If Too Much'";
   }
-
 };
-
 function CheckIfTooMuch(string){
 var words = string.split(',');
-
 if(words.length <= 3){
   return "Enjoy!";
 }else{
     return "Too Much!";
 }
-
-
 }
-
-
-
 }
-
-
 })();
